4820465656def8bd25ccb650f4ab2a54d4521a42
{krism01} <{krism01@naver.com}>
2023-02-16 04:02:04 +0900

    GetBuilder 사용하여 데이터 처리 - todo: 단, 불필요한 업데이트 과정 개선하기

--- lib/main1/count_page.dart
+++ lib/main1/count_page.dart
@@ -1,26 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:second_project/main1/count_controller/count_controller.dart';
 
-class CountPage extends StatefulWidget {
-  const CountPage({Key? key}) : super(key: key);
-
-  @override
-  State<CountPage> createState() => _CountPageState();
-}
-
-class _CountPageState extends State<CountPage> {
-  int count = 0;
-
-  void countUp() {
-    setState(() {
-      count++;
-    });
-  }
-
-  void countDown() {
-    setState(() {
-      count--;
-    });
-  }
+class CountPage extends StatelessWidget {
+  CountPage({Key? key}) : super(key: key);
+  final CountController countController = Get.put(CountController());
 
   @override
   Widget build(BuildContext context) {
@@ -37,14 +21,20 @@ class _CountPageState extends State<CountPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(count.toString(), style: const TextStyle(fontSize: 36)),
+              GetBuilder<CountController>(
+                builder: (controller) {
+                  // note: test update: 같은 값이여도 화면 rebuild 이뤄지고 있다. 개선방법은 ?
+                  print('Update!!!');
+                  return Text(controller.count.toString(), style: const TextStyle(fontSize: 36));
+                }
+              ),
               const SizedBox(height: 20),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   ElevatedButton(
                     onPressed: () {
-                      countDown();
+                      countController.countDown();
                     },
                     style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.red,
@@ -57,7 +47,7 @@ class _CountPageState extends State<CountPage> {
                   const SizedBox(width: 25),
                   ElevatedButton(
                     onPressed: () {
-                      countUp();
+                      countController.countUp();
                     },
                     style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.blue,
@@ -67,7 +57,21 @@ class _CountPageState extends State<CountPage> {
                         minimumSize: const Size(100, 50)),
                     child: const Icon(Icons.add),
                   ),
+
                 ],
+              ),
+              const SizedBox(height: 15),
+              ElevatedButton(
+                onPressed: () {
+                  countController.countValue(5);
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.brown,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    minimumSize: const Size(100, 50)),
+                child: const Text('5로 변경하기',style: TextStyle(fontSize: 20)),
               ),
             ],
           ),
