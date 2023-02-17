7ee4f781de6e3632703b1ee77220affbd8f69cfe
{krism01} <{krism01@naver.com}>
2023-02-17 16:30:52 +0900

    make Table

---
+++ lib/main2/main2.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:second_project/main2/table_widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'table widget',
+      debugShowCheckedModeBanner: false,
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatefulWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  State<MyPage> createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: const Text('Flutter Tutorial',
+            style: TextStyle(color: Colors.black)),
+      ),
+      backgroundColor: Colors.white,
+      body: TableWidget(),
+    );
+  }
+}
