b68330277420dc70e056a6c1fb9c14f22483889b
MinJaehyun <43669992+MinJaehyun@users.noreply.github.com>
2023-07-11 16:29:11 +0900

    해당 페이지 에서만 snack bar msg 띄우는 방법

---
+++ main7/lib/1.share_scaffold/product_list1.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:main7/main.dart';
+
+class ProductList extends StatelessWidget {
+  const ProductList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Builder(
+          builder: (context) {
+            return Card(
+              child: ListTile(
+                leading: IconButton(onPressed: (){
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+                    return MyPage();
+                  },),);
+                }, icon: Icon(Icons.home_filled)),
+                title: Text('MackBook Pro'),
+                trailing: IconButton(onPressed: (){
+                  final snackBar = SnackBar(
+                    content: const Text('장바구니에 상품을 담았습니다'),
+                    action: SnackBarAction(
+                      label: 'Undo',
+                      onPressed: () {
+                        // some code to undo
+                      },
+                    ),
+                  );
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                }, icon: Icon(Icons.add_shopping_cart)),
+              ),
+            );
+          }
+        ),
+      ),
+    );
+  }
+}
