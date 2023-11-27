a79776fdcd2d06bbfb1d0baaae238db1b1084bb1
MinJaehyun <43669992+MinJaehyun@users.noreply.github.com>
2023-11-28 00:25:26 +0900

    update main5

--- main5/lib/main.dart
+++ main5/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:main5/screen/introduction_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Introduction Screen',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: null,
+      home: IntroductionPage(),
     );
   }
 }
